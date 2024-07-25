8f0d609963212f8c0f2daa4103ea4b4b9d97dbe2
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2024-07-25 23:07:30 +0900

    authorize people to show rest

---
+++ app/policies/restaurant_policy.rb
@@ -0,0 +1,22 @@
+class RestaurantPolicy < ApplicationPolicy
+  # NOTE: Up to Pundit v2.3.1, the inheritance was declared as
+  # `Scope < Scope` rather than `Scope < ApplicationPolicy::Scope`.
+  # In most cases the behavior will be identical, but if updating existing
+  # code, beware of possible changes to the ancestors:
+  # https://gist.github.com/Burgestrand/4b4bc22f31c8a95c425fc0e30d7ef1f5
+
+  class Scope < ApplicationPolicy::Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+  end
+
+  def show?
+    # In here, we write the logic of who is able to run #show for this resource (a restaurant)
+    # If it is a diary app, should be the owner
+    # If it is a paid news article, maybe who are paid member
+    # If it is just a restaurant info app, everyone
+    return true
+  end
+end
