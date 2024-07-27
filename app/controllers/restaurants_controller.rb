83cf34f995c9669169aad51142e873a9950a4463
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2024-07-27 09:50:47 +0900

    anybody can create restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,11 +8,14 @@ class RestaurantsController < ApplicationController
 
   # GET /restaurants/1
   def show
+    authorize @restaurant
   end
 
   # GET /restaurants/new
   def new
     @restaurant = Restaurant.new
+
+    authorize @restaurant
   end
 
   # GET /restaurants/1/edit
@@ -23,6 +26,8 @@ class RestaurantsController < ApplicationController
   def create
     @restaurant = Restaurant.new(restaurant_params)
     @restaurant.user = current_user
+
+    authorize @restaurant
 
     if @restaurant.save
       redirect_to @restaurant, notice: "Restaurant was successfully created."
